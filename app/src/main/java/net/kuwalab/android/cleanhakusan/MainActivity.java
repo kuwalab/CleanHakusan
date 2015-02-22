dad6ddcc2f289c578c93621cd808d2938639c3c0
kuwalab <arata.shinichi@gmail.com>
2015-02-22 21:12:38 +0900

    ActionBarのスタイルの設定

--- app/src/main/java/net/kuwalab/android/cleanhakusan/MainActivity.java
+++ app/src/main/java/net/kuwalab/android/cleanhakusan/MainActivity.java
@@ -1,7 +1,7 @@
 package net.kuwalab.android.cleanhakusan;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
