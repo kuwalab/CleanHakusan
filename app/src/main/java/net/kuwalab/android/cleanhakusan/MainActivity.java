e4450a5bd7ce24ace17219db2146c7307b6dab81
kuwalab <arata.shinichi@gmail.com>
2015-02-25 18:32:02 +0900

    余分な空行の除去

--- app/src/main/java/net/kuwalab/android/cleanhakusan/MainActivity.java
+++ app/src/main/java/net/kuwalab/android/cleanhakusan/MainActivity.java
@@ -52,7 +52,6 @@ public class MainActivity extends ActionBarActivity {
         ));
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
