2b31132df72ce90033e9b31125c0fb814c1bf271
kuwalab <arata.shinichi@gmail.com>
2015-02-25 19:01:01 +0900

    レイアウトの調整

--- app/src/main/java/net/kuwalab/android/cleanhakusan/MainActivity.java
+++ app/src/main/java/net/kuwalab/android/cleanhakusan/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends ActionBarActivity {
     private RequestQueue requestQueue;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
