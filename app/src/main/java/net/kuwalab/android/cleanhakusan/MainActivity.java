337dfe16615d3f760612307548a9f68320d5b385
kuwalab <arata.shinichi@gmail.com>
2015-02-26 21:19:26 +0900

    フラグメントに処理を委譲

--- app/src/main/java/net/kuwalab/android/cleanhakusan/MainActivity.java
+++ app/src/main/java/net/kuwalab/android/cleanhakusan/MainActivity.java
@@ -4,24 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 
 public class MainActivity extends ActionBarActivity {
-    private RequestQueue requestQueue;
-
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,28 +21,6 @@ public class MainActivity extends ActionBarActivity {
 
         FragmentManager manager = getSupportFragmentManager();
         MainFragment mainFragment = (MainFragment) manager.findFragmentById(R.id.mainFragment);
-
-        String url = "https://cleanhakusan.herokuapp.com/api/version";
-
-        requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(new JsonObjectRequest(Request.Method.GET, url, null,
-            new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    // JSONObjectのパース、List、Viewへの追加等
-                    try {
-                        Log.i("#####", response.getString("version"));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            },
-            new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                }
-            }
-        ));
     }
 
     @Override
--- app/src/main/java/net/kuwalab/android/cleanhakusan/MainFragment.java
+++ app/src/main/java/net/kuwalab/android/cleanhakusan/MainFragment.java
@@ -1,24 +1,51 @@
 package net.kuwalab.android.cleanhakusan;
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainFragment extends Fragment {
-
-    private OnFragmentInteractionListener mListener;
-
-    public MainFragment() {
-    }
+    private RequestQueue requestQueue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String url = "https://cleanhakusan.herokuapp.com/api/version";
+
+        requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(new JsonObjectRequest(Request.Method.GET, url, null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    // JSONObjectのパース、List、Viewへの追加等
+                    try {
+                        Log.i("#####", response.getString("version"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                }
+            }
+        ));
     }
 
     @Override
@@ -36,10 +63,5 @@ public class MainFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        public void onFragmentInteraction(Uri uri);
     }
 }
